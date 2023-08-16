00064933559fbcb64e64a3dfa9891823a903e199
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-16 10:01:31 +0100

    Remove bio from Post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_082041) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_085847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_082041) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
