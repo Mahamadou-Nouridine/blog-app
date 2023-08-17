18f17c6938590fd7df8f081d10e6f1c40db4fdbd
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-17 14:39:45 +0100

    Add Comment  Model

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  after_create :update_counter
+  after_destroy :update_counter
+
+  def update_counter
+    post.comments_count = post.comments.count
+    post.save
+  end
+end
