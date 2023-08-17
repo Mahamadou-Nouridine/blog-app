71fbbc4ceb30ec8e892e3f340ae8d5654f985f12
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-17 15:43:03 +0100

    Change user property to author in likes and comments models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :author, class_name= 'User'
+  belongs_to :author, class_name= 'User'
   after_create :update_counter
   after_destroy :update_counter
 
