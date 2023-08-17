7948f98f14a8f5bb61f05eeef2614312b5b003f2
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-17 14:39:17 +0100

    Add user Model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def recent_post
+    posts.order(created_at: :desc).limit(3)
+  end
+end
