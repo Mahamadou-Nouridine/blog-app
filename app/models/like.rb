ad0e085378e0b60988c266aff0743ae157996a7e
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-17 17:11:43 +0100

    Add user class_name to like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :author, 'User'
+  belongs_to :author, class_name: 'User'
   belongs_to :post
   after_create :update_counter
   after_destroy :update_counter
