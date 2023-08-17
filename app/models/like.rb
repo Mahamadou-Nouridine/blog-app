1c3aee9d6749c62d3cdeb4af32485bd91d9b03e9
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-17 15:44:20 +0100

    Make like and comment models update_counter method private

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,6 +4,8 @@ class Like < ApplicationRecord
   after_create :update_counter
   after_destroy :update_counter
 
+  private
+
   def update_counter
     post.comments_count = post.comments.count
     post.save
