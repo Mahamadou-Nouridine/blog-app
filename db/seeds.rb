ce066636e4b54c359f23709ca13007d5f59dcd92
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-23 15:33:22 +0100

    Add working into the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# user1 = User.create(name: "Nouridine", photo: "https://avatars.githubusercontent.com/u/98597449?v=4", bio: "Rails developer")
+# user2 = User.create(name: "Mahamadou", photo: "https://avatars.githubusercontent.com/u/98597449?v=4", bio: "MERN developer")
+ users = User.all
+
+
+
+post = Post.create(title: "Rails seeding guide", text: "rails seeding guide description", author_id: users[0].id)
+users[1].likes.create(post_id: post.id)
+users[1].comments.create(post_id: post.id, text: "I like this article")
