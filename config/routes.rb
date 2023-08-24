7a91df126d8a2b4cfee5313c35889d34dbad1c04
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-24 17:37:08 +0100

    Change routing from the use of verbs to the use of ressources helper

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  get "/users/:id", to: "users#show"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
+  # get "/users/:id", to: "users#show"
+  # get "/users/:user_id/posts", to: "posts#index"
+  # get "/users/:user_id/posts/:id", to: "posts#show"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+   end
+  end
 end
