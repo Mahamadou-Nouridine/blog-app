fa8803b92769376cc6d47ba89065790c5744cf2f
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-25 12:13:12 +0100

    Remove unused code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,18 +16,7 @@ class PostsController < ApplicationController
 
   def create
     user = current_user
-    # data = params.require(:post).permit(:text, :title)
-    # fs =qdz.it
     Post.create(title: params[:title], text: params[:text], author_id: user.id)
-
-    # post.author_id = user.id
-    # post.comments_count = 0
-    # post.likes_count = 0
-    # post.save
     redirect_to "/users/#{user.id}/posts", notice: 'new post created successfully'
-    # if post.save
-    # else
-    #   render 'new'
-    # end
   end
 end
